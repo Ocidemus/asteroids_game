1fd62942e03173ea0c6a36d18459802b7906de93
Ocidemus <codeofascendence@gmail.com>
2025-05-30 11:21:01 +0000

    A: add base files

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    print("Starting Asteroids!")
+    screen= pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while(1):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen,"black")
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
